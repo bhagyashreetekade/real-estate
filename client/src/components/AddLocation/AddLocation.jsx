35149efdbca24b30f3ffeaa9bdeca0aa503ce873
Bhagyashree Ganesh Tekade <bhagyashreetekade609@gmail.com>
2023-09-04 11:26:16 +0530

    add bookings

---
+++ client/src/components/AddLocation/AddLocation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddLocation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddLocation
--- client/src/components/AddPropertyModal/AddPropertyModal.jsx
+++ client/src/components/AddPropertyModal/AddPropertyModal.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Modal, Stepper } from "@mantine/core";
+import AddLocation from "../AddLocation/AddLocation";
+import {useAuth0} from "@auth0/auth0-react"
+const AddPropertyModal = ({ opened, setOpened }) => {
+  const [active, setActive] = useState(0);
+  const {user}=useAuth0()
 
-const AddPropertyModal = () => {
+  const [propertyDetails, setPropertyDetails] = useState({
+    title: "",
+    description: "",
+    price: 0,
+    address: "",
+    city: "",
+    country: "",
+    image: null,
+    facilities: {
+      bedrooms: 0,
+      parkings: 0,
+      bathrooms: 0,
+    },
+    userEmail:user?.email,
+  });
+
+  const nextStep=()=>{
+    setActive((current)=>(current<4?current+1:current))
+  }
+  const prevStep=()=>{
+    setActive((current)=>(current>4?current-1:current))
+  }
+
   return (
-    <div>
-      AddPropertyModal
-    </div>
-  )
-}
+    <Modal
+      opened={opened}
+      onClose={() => setOpened(false)}
+      closeOnClickOutside
+      size={"90rem"}
+    >
+      <Container h={"40rem"} w={"100%"}>
+        <Stepper
+          active={active}
+          onStepClick={setActive}
+          breakpoint="sm"
+          allowNextStepsSelect={false}
+        >
+          <Stepper.Step label="Location" description="Address">
+            <AddLocation nextStep={nextStep} propertyDetails={propertyDetails} setPropertyDetails={setPropertyDetails}/>
+          </Stepper.Step>
+          <Stepper.Step label="Second step" description="Verify email">
+            Step 2 content: Verify email
+          </Stepper.Step>
+          <Stepper.Step label="Final step" description="Get full access">
+            Step 3 content: Get full access
+          </Stepper.Step>
+          <Stepper.Completed>
+            Completed, click back button to get to previous step
+          </Stepper.Completed>
+        </Stepper>
+      </Container>
+    </Modal>
+  );
+};
 
-export default AddPropertyModal
+export default AddPropertyModal;
